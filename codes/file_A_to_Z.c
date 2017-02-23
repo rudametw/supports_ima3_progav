2630b4dcf752d2f8b63efd860734ca91c405ab43
rudametw <rudametkin@gmail.com>
2017-02-23 15:52:53 +0100

    Updated simple A to Z save to file test.
    Former-commit-id: 02efeccdf8d4516cfa8d1386c5032b6e8e9cee2c

--- codes/file_A_to_Z.c
+++ codes/file_A_to_Z.c
@@ -10,17 +10,26 @@ void print_char(mychar c){
 int main() {
 
 	mychar c;
+	unsigned long huit_bytes ;
 
-	for (c = 65; c < 70; c++)
+	for (c = 65; c < 95; c++){
     	printf("%d:%c ", c,c);
+	}
 	printf("\n\n\n");
 
 	FILE *fp1;
-	if ((fp1 = fopen("file_A_to_Z.txt", "wb")) != NULL) {
-		for (c = 65; c < 70; c++)
-			fwrite(&c,1,1,fp1);
-		long deux = 75;
-		fwrite(&deux,sizeof deux,1,fp1);
+	if ((fp1 = fopen("file_A_to_Z.dat", "w+")) != NULL) {
+		for (c = 65; c < 95; c++)
+			fwrite(&c,1,1,fp1)
+;
+		/*fseek(fp1,0,SEEK_SET);*/
+		/*long deux = 75;*/
+		huit_bytes = 16705; //Ajoute deux A (01000001)
+		huit_bytes = 02024050120240501; //Ajoute deux 5
+		huit_bytes = 1094795585 ; //Équivalent 4xA
+		huit_bytes = 0x4141414141414141 ; //Équivalent 8xA
+		printf("Variable huit_bytes prends %lu bytes en mémoire with value %ld %lx \n",sizeof huit_bytes, huit_bytes, huit_bytes);
+		fwrite(&huit_bytes,sizeof huit_bytes,1,fp1);
 		fwrite("xxx\n",1,4,fp1);
 	}
 	if (fp1 != NULL) fclose(fp1);
